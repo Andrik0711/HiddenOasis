409cdf79c7067305ea3302e463a1ea54fcf963c8
Andrik <mord.71gamer@gmail.com>
2023-03-06 23:45:45 -0600

    nueva version

--- ExamenU2/Examen/js/login_global.js
+++ ExamenU2/Examen/js/login_global.js
@@ -7,11 +7,11 @@ function validarAcceso() {
     // Comparamos los valores con los datos de acceso correctos
     if (usuario === "jperez" && contrasena === "mipssw") {
         // Si los datos son correctos, redireccionamos a la página de inicio del cliente
-        window.location.href = "./Examen/menu.html";
+        window.location.href = "/ExamenU2/Examen/Examen/menu.html";
     }
     if (usuario === "vendedor" && contrasena === "mipssw-vend") {
         // Si los datos son correctos, redireccionamos a la página de inicio del vendedor
-        window.location.href = "./Examen/menu.html";
+        window.location.href = "/ExamenU2/Examen/Examen/menu.html";
     } else {
         // Si los datos son incorrectos, mostramos un modal de error
         $("#modalError").modal("show");
